c18f44c9f6a7c2ab9becbdc93ff86fcaab62b5e7
tommy <tomastrg22@gmail.com>
2022-11-23 18:29:56 +0100

    wp

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ import "~/styles/index.scss";
 import 'uno.css'
 
 // If you want to use ElMessage, import it.
-import "element-plus/theme-chalk/src/message.scss"
+// import "element-plus/theme-chalk/src/message.scss"
 
 const app = createApp(App);
 // app.use(ElementPlus);
